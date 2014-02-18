213dd6f259d7297444e56bfdc14dfc1c76ebbb68
Heidi Petersen <heidipetersen@gmail.com>
2014-02-17 19:11:39 -0800

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diveprograms::Application.config.secret_key_base = '0ce0dd99bdad7bcaa3aed5312c8d0059a9a131d2af85aa55f582f039c248d36f583ebfe97b6b81d50d3d9980e8e820bcbff8f385e47e5ded03763fa69c43bb47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Diveprograms::Application.config.session_store :cookie_store, key: '_diveprograms_session'
